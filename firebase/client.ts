57e2d0bd1708165b1786067e92762bc01d0e9ee1
monnsevega <monsev0301@gmail.com>
2026-04-20 22:34:51 -0600

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,13 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./config";
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyA8IY_Wbh33pdqaaY2kKO54CGifi-fEdPs",
+  authDomain: "web-218500.firebaseapp.com",
+  projectId: "web-218500",
+  appId: "1:539802319607:web:dc04682efd3ffa857531f2",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
